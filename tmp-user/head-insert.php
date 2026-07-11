3c4a1ed1c4f094d4854f07dff9c6822ae092ef3e
yuso <95833220+yuso21@users.noreply.github.com>
2026-07-11 19:00:31 +0900

    feat: Add Microsoft Clarity tracking script to head-insert

--- tmp-user/head-insert.php
+++ tmp-user/head-insert.php
@@ -5,6 +5,13 @@
 ?>
 <?php if (!is_user_administrator()) :
 //管理者を除外してカウントする場合は以下に挿入 ?>
-
+<!-- Microsoft Clarity -->
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "xkq3mqvi92");
+</script>
 <?php endif; ?>
 <?php //全ての訪問者をカウントする場合は以下に挿入 ?>
